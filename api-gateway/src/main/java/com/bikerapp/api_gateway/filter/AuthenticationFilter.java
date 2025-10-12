d264a9c8e0c8b216984616dbdbcfee0f4e35b1d8
bkr4wczynski <bkrawczynski@protonmail.com>
2025-10-12 12:08:49 +0200

    gateway receives token

--- api-gateway/src/main/java/com/bikerapp/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/bikerapp/api_gateway/filter/AuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.bikerapp.api_gateway.filter;
 
+import org.apache.http.HttpHeaders;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ public class AuthenticationFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         System.out.println("Request received: "+exchange.getRequest().getPath());
+        String cookie = String.valueOf(exchange.getRequest().getCookies().getFirst("token"));
+
+        System.out.println("Cookie: "+cookie);
         return chain.filter(exchange);
     }
 }
--- web-service/src/main/java/com/bikerapp/web_service/controller/auth/AuthController.java
+++ web-service/src/main/java/com/bikerapp/web_service/controller/auth/AuthController.java
@@ -2,7 +2,12 @@ package com.bikerapp.web_service.controller.auth;
 
 import com.bikerapp.web_service.model.dto.auth.LoginDTO;
 import com.bikerapp.web_service.service.AuthConnectionService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +33,15 @@ public class AuthController {
     }
 
     @PostMapping("/sign-in")
-    public String signIn(@ModelAttribute LoginDTO loginDTO) {
+    public String signIn(@ModelAttribute LoginDTO loginDTO, HttpServletResponse response) {
         String token = authConnectionService.getToken(loginDTO);
         System.out.println(token);
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(15*60);
+        response.addCookie(cookie);
         return "redirect:http://localhost:8765/web/my-profile";
     }
 }
