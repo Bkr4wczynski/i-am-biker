3798e2b0c024ece56852c4553e7cf9b69e9eb441
bkr4wczynski <bkrawczynski@protonmail.com>
2025-10-12 11:35:48 +0200

    intercepting requests in gateway

--- web-service/src/main/java/com/bikerapp/web_service/controller/auth/AuthController.java
+++ web-service/src/main/java/com/bikerapp/web_service/controller/auth/AuthController.java
@@ -8,9 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
     private final AuthConnectionService authConnectionService;
 
