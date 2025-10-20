f6b3bcf26cefab1e57f95340cc20e9438898f5e2
bkr4wczynski <bkrawczynski@protonmail.com>
2025-10-20 19:21:55 +0200

    safety tips content

--- web-service/src/main/java/com/bikerapp/web_service/controller/profile/ProfilePageController.java
+++ web-service/src/main/java/com/bikerapp/web_service/controller/profile/ProfilePageController.java
@@ -1,12 +1,10 @@
 package com.bikerapp.web_service.controller.profile;
 
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@AllArgsConstructor
 public class ProfilePageController {
 
     @GetMapping("/my-profile")
